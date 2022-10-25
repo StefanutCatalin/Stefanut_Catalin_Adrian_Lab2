d7e70119af371956e6b8721b92038be26995d325
Cata <Cata@DESKTOP-G725SUH>
2022-10-25 14:13:48 +0300

    Add project files.

---
+++ Data/Stefanut_Catalin_Adrian_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Stefanut_Catalin_Adrian_Lab2.Models;
+
+namespace Stefanut_Catalin_Adrian_Lab2.Data
+{
+    public class Stefanut_Catalin_Adrian_Lab2Context : DbContext
+    {
+        public Stefanut_Catalin_Adrian_Lab2Context (DbContextOptions<Stefanut_Catalin_Adrian_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Stefanut_Catalin_Adrian_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Stefanut_Catalin_Adrian_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
